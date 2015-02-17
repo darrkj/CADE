4d7c9d7fb7fd76296a26dd2e14b4a1c10eabe45e
Kenny Darrell <darrelkj@gmail.com>
2015-02-16 20:41:00 -0500

    add package r file

---
+++ CADE.R
@@ -0,0 +1,8 @@
+#' CADE: A package for using the cade algorithm to find outliers.
+#'
+#' The CADE package provides functions to find outliers and make uniform 
+#' distributions of each varaible type.
+#' 
+#' @docType package
+#' @name CADE
+NULL
