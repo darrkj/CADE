72342a79f28cbe8882a1542950164c94ff1f86a2
Kenny Darrell <darrelkj@gmail.com>
2015-02-16 19:40:42 -0500

    bunch of changes to make work as a package

--- R/CADE.R
+++ R/CADE.R
@@ -1,42 +1,26 @@
 
 
-
-
-# TODO: guid could be character, {19f3lt:df2fs42f:e12r53tad:...} a la sure view,
-# this will work for most text file type unique identifiers.
-#is.guid <- function(x) {
-#  is.integer(x) & length(unique(x)) == length(x)
-#}
-
-# TODO: Add method to use different classifier
-# TODO: Should bounds on uniform be scaled past min/max range
-# prop allows you to throttle the 50/50 split, higher means more fake data.
-# TODO: cade add ability to work with data that already has a y variable
-
 #' The main CADE function
 #'
 #' This function calls and runs CADE
 #' @param df The data frame in question
 #' @param prop The proportion of true to false cases
-#' @param numTree The number of tree to create in the random forest
 #' @param skip Fields that should not be included 
 #' @keywords anonaly detection
 #' @export
 #' @examples
 #' cade(cars)
 
-cade <- function(df, prop = 1, numTree = 500, skip = c()) {
+cade <- function(df, prop = 1, skip = c(), cutoff = .5, ...) {
   stopifnot(is.data.frame(df))
   #TODO: get better na action
+  #TODO: Should convert chars to factors
   df <- df[complete.cases(df), ]
   
   ids <- df[, skip]
   real <- if (length(skip) > 0) df[, -(which(names(df) %in% skip))] else df
   
-  # Take out any id fields
-  # real <- df[, !unlist(lapply(df, is.guid))]
-  
-  # create similar but uniform data
+  # Create similar but uniform data
   fake <- as.data.frame(lapply(real, function(x) uniform(x, length(x) * prop)))
   
   # Label the data you started with as negative outcomes.
@@ -47,26 +31,20 @@ cade <- function(df, prop = 1, numTree = 500, skip = c()) {
   # Combine both the real and the fake data.
   data <- rbind(real, fake)
   
-  library(randomForest)
-  # build classifier
-  
-  tree <- randomForest(as.factor(y) ~ ., data = data, ntree = numTree, 
-                       importance = TRUE)
+  # Build classifier
+  tree <- randomForest::randomForest(as.factor(y) ~ ., data = data, 
+                                     importance = TRUE)
   
   vars <- names(attr(tree$terms, 'dataClasses')[-1])
   prop <- tree$importance[, 1] / sum(tree$importance[, 1])
   
   # the classifier probabilities
   # TODO: save time only predict real cases
-  df$prob <- predict(tree, newdata = data, type = 'prob')[1:nrow(real), 2]
+  df$prob <- predict(tree, newdata = df, type = 'prob')[, 2]
   df$prob <- df$prob / (1 - df$prob)
-  
-  prop <- prop * df[, vars]
-  names(prop) <- paste('prop.', names(prop), sep = '')
-  
-  df <- cbind(df, prop)
-  df <- df[df$prob > 0, ]
-  df
+
+  df$outlier <- ifelse(df$prob > cutoff, 1, 0)
+  df[order(-df$prob), ]
 }
 
 
